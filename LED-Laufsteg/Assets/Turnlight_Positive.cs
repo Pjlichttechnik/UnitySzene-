b48f137a0783868a8ffe010087247aa10a9cc378
houcemRabhi <96446880+Pjlichttechnik@users.noreply.github.com>
2022-01-13 19:51:29 +0100

    LVKs+C#Scripten

---
+++ LED-Laufsteg/Assets/Turnlight_Positive.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turnlight_Positive : MonoBehaviour
+{
+    public GameObject light0;
+    public GameObject light1;
+    public GameObject light2;
+    public GameObject light3;
+    public GameObject light4;
+    private bool on = false;
+    // Use this for initialization
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && !on)
+        {
+            light0.SetActive(true);
+            light1.SetActive(true);
+            light2.SetActive(true);
+            light3.SetActive(true);
+            light4.SetActive(true);
+            on = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.C) && on)
+        {
+            light0.SetActive(false);
+            light1.SetActive(false);
+            light2.SetActive(false);
+            light3.SetActive(false);
+            light4.SetActive(false);
+            on = false;
+        }
+    }
+}
